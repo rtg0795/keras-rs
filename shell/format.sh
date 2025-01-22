86affe7b0d0ade778c9370ec5bc670e075e5d1e0
Abheesht <sharmabhee@gmail.com>
2025-01-23 00:32:40 +0530

    Add pre-commit hooks (#11)
    * Add pre-commit hooks
    
    * Sync with GH Actions
    
    * Run pre-commit cmd post api_gen only for api directory
    
    * Separate out formatting and linting
    
    * Add api gen to pre-commit hook
    
    * Add pre-commit install stmt to actions.yml
    
    * Add pre-commit to requirements.txt

--- shell/format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Euo pipefail
-
-base_dir=$(dirname $(dirname $0))
-targets="${base_dir}"
-
-ruff check --config "${base_dir}/pyproject.toml" --fix ${targets}
-ruff format --config "${base_dir}/pyproject.toml" ${targets}
-mypy --config-file "${base_dir}/pyproject.toml" ${targets}
