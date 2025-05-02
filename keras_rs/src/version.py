47f8fbcd232f4136c0d3403d28bf53c60fb2c664
Abheesht <sharmabhee@gmail.com>
2025-05-02 08:18:18 +0530

    Change master version to 0.1.1.dev0 (#79)

--- keras_rs/src/version.py
+++ keras_rs/src/version.py
@@ -1,7 +1,7 @@
 from keras_rs.src.api_export import keras_rs_export
 
 # Unique source of truth for the version number.
-__version__ = "0.1.1"
+__version__ = "0.1.1.dev0"
 
 
 @keras_rs_export("keras_rs.version")
